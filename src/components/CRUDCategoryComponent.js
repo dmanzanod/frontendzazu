193286b3d7df49597320df1aa94267b5e82e5ca4
akusei <akusei24@gmail.com>
2023-04-23 18:41:10 -0400

    routes redirect Category

--- src/components/CRUDCategoryComponent.js
+++ src/components/CRUDCategoryComponent.js
@@ -4,7 +4,9 @@ import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from '@mui/icons-material/Edit';
 import { red } from "@mui/material/colors";
-const CRUDCategoryComponent = () => {
+import { useNavigate } from "react-router-dom";
+const CRUDCategoryComponent = ({id}) => {
+    const navigate=useNavigate()
   return (
     <Box
       sx={{
@@ -14,11 +16,12 @@ const CRUDCategoryComponent = () => {
         justifyContent: "space-evenly",
       }}
     >
-      <IconButton aria-label="update" color='primary.light'>
-        <EditIcon />
+      <IconButton aria-label="update" color='primary.light' onClick={()=>navigate(`/editCategory/${id}`)}>
+        <EditIcon color='primary'/>
       </IconButton>
-      <IconButton aria-label="delete" >
-        <DeleteIcon color={red[600]}/>
+      <IconButton aria-label="delete" onClick={()=>navigate(`/deleteCategory/${id}`)}>
+        <DeleteIcon sx={{color:red[400]}}/>
+
       </IconButton>
     </Box>
   );
--- src/pages/ProductService.js
+++ src/pages/ProductService.js
@@ -1,6 +1,6 @@
 import { Box, Button, Typography } from '@mui/material'
-import { grey } from '@mui/material/colors'
-import { DataGrid } from '@mui/x-data-grid'
+import { blue, grey } from '@mui/material/colors'
+import { DataGrid,esES } from '@mui/x-data-grid'
 import React, { useEffect, useState } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import Principal from './Principal';
@@ -8,14 +8,17 @@ import { useParams } from 'react-router-dom';
 import { getServices } from '../services/service';
 import CRUDCategoryComponent from '../components/CRUDCategoryComponent';
 const ProductService = () => {
-    
+    const [selectedRow,setSelectedRow]=useState([])
     const getCategoryName=(params)=>{
         return params.row.categoryId.name
+    }
+    const getServiceId=(params)=>{
+        return params.row._id
     }
     const formatPrice=(params)=>{
         return `${params.row.price} ${params.row.coin}`
     }
-    const columns=[{field: 'name',width:'200', headerName:'Nombre'},{field:'category', width:'200', headerName:'Categoría', valueGetter:getCategoryName},{field:'price',headerName:'Precio', valueGetter: formatPrice},{field:'operations',headerName:'Acciones', renderCell:()=><CRUDCategoryComponent/>}]
+    const columns=[{field: 'name',width:'200', headerName:'Nombre'},{field:'category', width:'200', headerName:'Categoría', valueGetter:getCategoryName},{field:'price',headerName:'Precio', valueGetter: formatPrice},{field:'operations',headerName:'Acciones', renderCell:(params)=><CRUDCategoryComponent id={params.row._id}/>}]
     const [services,setServices]=useState([])
     const {id}= useParams()
     useEffect(()=>{
@@ -30,32 +33,35 @@ const ProductService = () => {
 
   return (
     <Principal>
-        <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', width:'100%',gap:'20px', backgroundColor:'#fff'}}>
+        <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', width:{xs:'calc(100% - 60px)',sm:'100%'},gap:'20px', backgroundColor:'#fff'}}>
         
-                <Typography variant='h3' color='primary'sx={{mt:10}}>Categorías</Typography>
+                <Typography variant='h3' color='primary'sx={{mt:10}}>Servicios</Typography>
             
                
                 
                
-            <Box sx={{width:{xs:'100%',sm:'80%'},maxHeight:'450px', marginBottom:{xs:'200px',sm:'180px'}}}>
+            <Box sx={{width:{xs:'100%',sm:'80%'}, marginBottom:{xs:'200px',sm:'180px'}, display:'flex',flexDirection:'column'}}>
                 <Button variant='contained' sx={{ color:'#fff', borderRadius:'8px', width:'120px',alignSelf:'flex-start', m:2}}startIcon={<AddIcon />}>
                      
-                     Nueva</Button>
+                     Nuevo</Button>
             <DataGrid
             sx={{mt:2,
-                
+                marginInline:'auto',
+                width:'90%',
                 borderColor: 'primary.light',
-                backgroundColor:grey[100],
+                backgroundColor:blue[50],
             '& .MuiDataGrid-cell:hover': {
                 color: 'primary.main',
               }}}
-            
+              localeText={esES.components.MuiDataGrid.defaultProps.localeText}
         rows={services}
         getRowId={(row) => row._id}
         columns={columns}
         pageSize={3}
         rowsPerPageOptions={[5]}
-        
+        rowLength={10}
+        onRowSelectionModelChange={(newRowSelected)=>{console.log(newRowSelected);setSelectedRow(newRowSelected)}}
+        rowSelectionModel={selectedRow}
       />
       </Box>
             
